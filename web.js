c189f723d95e93b2f370b1ed1bc27f1fe9f24173
Dominic Boehler <dominic.boehler@gmx.de>
2013-07-20 15:20:30 +0200

    added port 8080 on local mac

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var rest = require('restler');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
@@ -8,7 +9,7 @@ app.get('/', function(request, response) {
   response.send(content);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
